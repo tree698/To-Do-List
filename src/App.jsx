d609cf6c17bc103cff600b7b8bbbab0e69a34199
Chanwoo <tree698@gmail.com>
2023-01-05 16:47:45 +0900

    add the functionality of dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Items from './components/Items/Items';
 import Nav from './components/Nav/Nav';
+import { DarkModeProvider } from './context/DarkModeContext';
 
 const filters = ['all', 'active', 'completed'];
 
@@ -8,9 +9,9 @@ export default function App() {
   const [filter, setFilter] = useState(filters[0]);
 
   return (
-    <>
+    <DarkModeProvider>
       <Nav filters={filters} filter={filter} onFilterChange={setFilter} />
       <Items filter={filter} />
-    </>
+    </DarkModeProvider>
   );
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,16 @@
-import { BsFillMoonFill } from 'react-icons/bs';
+import { HiMoon, HiSun } from 'react-icons/hi';
 import React from 'react';
 import styles from './Nav.module.css';
+import { useDarkMode } from '../../context/DarkModeContext';
 
 export default function Nav({ filters, filter, onFilterChange }) {
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
   return (
     <nav className={styles.nav}>
-      <button>
-        <BsFillMoonFill className={styles.icon} />
+      <button onClick={toggleDarkMode}>
+        {!darkMode && <HiMoon className={styles.icon} />}
+        {darkMode && <HiSun className={styles.icon} />}
       </button>
       <ul className={styles.menu__container}>
         {filters.map((value, index) => (
